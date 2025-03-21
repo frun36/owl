8634574daebdf2bb035743b0cf8dea715b19ac60
frun36 <franek.urbanski@gmail.com>
2025-03-21 21:21:42 +0100

    Basic blinky

--- main/hello_world_main.c
+++ main/hello_world_main.c
@@ -1,52 +1,37 @@
-/*
- * SPDX-FileCopyrightText: 2010-2022 Espressif Systems (Shanghai) CO LTD
- *
- * SPDX-License-Identifier: CC0-1.0
- */
-
-#include <stdio.h>
 #include <inttypes.h>
-#include "sdkconfig.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "esp_chip_info.h"
-#include "esp_flash.h"
-#include "esp_system.h"
+#include "driver/gpio.h"
+#include "esp_log.h"
+
+#define BOARD_LED 2
+
+static const char* TAG = "HELLO";
+
+static uint8_t s_led_state = 0;
+
+static void configure_led(void)
+{
+    ESP_LOGI(TAG, "Configuring GPIO LED!");
+    gpio_reset_pin(BOARD_LED);
+    /* Set the GPIO as a push/pull output */
+    gpio_set_direction(BOARD_LED, GPIO_MODE_OUTPUT);
+}
+
+static void blink_led(void)
+{
+    gpio_set_level(BOARD_LED, s_led_state);
+}
 
 void app_main(void)
 {
-    printf("Hello world!\n");
+    configure_led();
 
-    /* Print chip information */
-    esp_chip_info_t chip_info;
-    uint32_t flash_size;
-    esp_chip_info(&chip_info);
-    printf("This is %s chip with %d CPU core(s), %s%s%s%s, ",
-           CONFIG_IDF_TARGET,
-           chip_info.cores,
-           (chip_info.features & CHIP_FEATURE_WIFI_BGN) ? "WiFi/" : "",
-           (chip_info.features & CHIP_FEATURE_BT) ? "BT" : "",
-           (chip_info.features & CHIP_FEATURE_BLE) ? "BLE" : "",
-           (chip_info.features & CHIP_FEATURE_IEEE802154) ? ", 802.15.4 (Zigbee/Thread)" : "");
-
-    unsigned major_rev = chip_info.revision / 100;
-    unsigned minor_rev = chip_info.revision % 100;
-    printf("silicon revision v%d.%d, ", major_rev, minor_rev);
-    if(esp_flash_get_size(NULL, &flash_size) != ESP_OK) {
-        printf("Get flash size failed");
-        return;
+    while (1) {
+        ESP_LOGI(TAG, "LED %s!", s_led_state == true ? "ON" : "OFF");
+        blink_led();
+        /* Toggle the LED state */
+        s_led_state = !s_led_state;
+        vTaskDelay(100 / portTICK_PERIOD_MS);
     }
-
-    printf("%" PRIu32 "MB %s flash\n", flash_size / (uint32_t)(1024 * 1024),
-           (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
-
-    printf("Minimum free heap size: %" PRIu32 " bytes\n", esp_get_minimum_free_heap_size());
-
-    for (int i = 10; i >= 0; i--) {
-        printf("Restarting in %d seconds...\n", i);
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
-    }
-    printf("Restarting now.\n");
-    fflush(stdout);
-    esp_restart();
 }
