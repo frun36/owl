fcb5a83a6c84ba66da1740efec3e3049d61a0954
frun36 <franek.urbanski@gmail.com>
2025-04-18 11:06:08 +0200

    Setup basic SoftAP

--- main/owl_main.c
+++ main/owl_main.c
@@ -1,5 +1,6 @@
 #include <assert.h>
 #include <inttypes.h>
+#include <string.h>
 
 #include "esp_log.h"
 
@@ -9,6 +10,14 @@
 
 #include "button_gpio.h"
 #include "iot_button.h"
+
+#include "esp_event.h"
+#include "esp_mac.h"
+#include "esp_wifi.h"
+
+#include "nvs_flash.h"
+#include "esp_netif.h"
+#include "esp_http_server.h"
 
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
@@ -38,6 +47,88 @@ static void button_long_press_cb(void *arg, void *usr_data)
     ESP_LOGW(TAG, "BUTTON_LONG_PRESS_START");
 }
 
+#define SOFTAP_SSID "OWL"
+#define SOFTAP_PASS "FR4#1UL4"
+#define SOFTAP_CH 6
+#define SOFTAP_MAX_CONN 4
+
+static void wifi_event_handler(void *arg, esp_event_base_t event_base, int32_t event_id, void *event_data)
+{
+    if (event_id == WIFI_EVENT_AP_STACONNECTED) {
+        wifi_event_ap_staconnected_t *event = (wifi_event_ap_staconnected_t *) event_data;
+        ESP_LOGI(TAG, "station " MACSTR " join, AID=%d", MAC2STR(event->mac), event->aid);
+    } else if (event_id == WIFI_EVENT_AP_STADISCONNECTED) {
+        wifi_event_ap_stadisconnected_t *event = (wifi_event_ap_stadisconnected_t *) event_data;
+        ESP_LOGI(TAG, "station " MACSTR " leave, AID=%d, reason=%d", MAC2STR(event->mac), event->aid, event->reason);
+    }
+}
+
+void wifi_init_softap(void)
+{
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+    esp_netif_create_default_wifi_ap();
+
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &wifi_event_handler, NULL, NULL));
+
+    // clang-format off
+    wifi_config_t wifi_config = {
+        .ap = {
+            .ssid = SOFTAP_SSID,
+            .ssid_len = strlen(SOFTAP_SSID),
+            .channel = SOFTAP_CH,
+            .password = SOFTAP_PASS,
+            .max_connection = SOFTAP_MAX_CONN,
+            .authmode = WIFI_AUTH_WPA2_PSK,
+            .pmf_cfg = {
+               .required = true,
+            },
+        },
+    };
+    // clang-format on
+
+    if (strlen(SOFTAP_PASS) == 0) {
+        wifi_config.ap.authmode = WIFI_AUTH_OPEN;
+    }
+
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_AP));
+    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_AP, &wifi_config));
+    ESP_ERROR_CHECK(esp_wifi_start());
+
+    ESP_LOGI(TAG, "Initialized Wi-Fi SoftAP. SSID: %s PASS: %s CH: %d", SOFTAP_SSID,
+             SOFTAP_PASS, SOFTAP_CH);
+}
+
+
+esp_err_t root_get_handler(httpd_req_t *req)
+{
+    const char* resp_str = "<!DOCTYPE html><html><head><title>OWL</title></head>"
+                           "<body><h1>Helou</h1></body></html>";
+    httpd_resp_send(req, resp_str, HTTPD_RESP_USE_STRLEN);
+    return ESP_OK;
+}
+
+httpd_uri_t root = {
+    .uri       = "/",
+    .method    = HTTP_GET,
+    .handler   = root_get_handler,
+    .user_ctx  = NULL
+};
+
+httpd_handle_t start_webserver(void)
+{
+    httpd_config_t config = HTTPD_DEFAULT_CONFIG();
+    httpd_handle_t server = NULL;
+
+    if (httpd_start(&server, &config) == ESP_OK) {
+        httpd_register_uri_handler(server, &root);
+    }
+    return server;
+}
+
 void app_main(void)
 {
     ESP_LOGI(TAG, "Helou");
@@ -59,4 +150,16 @@ void app_main(void)
     iot_button_register_cb(btn, BUTTON_SINGLE_CLICK, NULL, button_single_click_cb, NULL);
     iot_button_register_cb(btn, BUTTON_DOUBLE_CLICK, NULL, button_double_click_cb, NULL);
     iot_button_register_cb(btn, BUTTON_LONG_PRESS_START, NULL, button_long_press_cb, NULL);
+
+    //Initialize NVS
+    ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret);
+
+    ESP_LOGI(TAG, "ESP_WIFI_MODE_AP");
+    wifi_init_softap();
+    start_webserver();
 }
