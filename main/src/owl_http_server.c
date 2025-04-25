0e0895c5109454a5259c68534ae72f3092c7dcf7
frun36 <franek.urbanski@gmail.com>
2025-04-25 21:28:55 +0200

    SPIFFS and index.html loading

--- main/src/owl_http_server.c
+++ main/src/owl_http_server.c
@@ -2,24 +2,20 @@
 
 #include "esp_http_server.h"
 #include "esp_log.h"
+#include "esp_spiffs.h"
 
 static const char *TAG = "owl_http_server";
 
 static httpd_handle_t server_handle = NULL;
 static int ws_fd = -1;
 
+static char *index_html = NULL;
+static size_t index_html_size = 0;
+
 static esp_err_t root_get_handler(httpd_req_t *req)
 {
-    static const char *resp
-        = "<!DOCTYPE html><html><body>"
-          "<h1>OWL</h1>"
-          "<h3>Device history:</h3>"
-          "<script>"
-          "const ws = new WebSocket('ws://' + location.host + '/ws');"
-          "ws.onmessage = e => document.body.innerHTML += '<br>' + e.data;"
-          "</script>"
-          "</body></html>";
-    httpd_resp_send(req, resp, HTTPD_RESP_USE_STRLEN);
+    httpd_resp_set_type(req, "text/html");
+    httpd_resp_send(req, index_html, index_html_size);
     return ESP_OK;
 }
 
@@ -38,8 +34,11 @@ static esp_err_t ws_handler(httpd_req_t *req)
         return ESP_OK;
     }
 
-    httpd_ws_frame_t frame
-        = { .type = HTTPD_WS_TYPE_TEXT, .payload = NULL, .len = 0 };
+    httpd_ws_frame_t frame = {
+        .type = HTTPD_WS_TYPE_TEXT,
+        .payload = NULL,
+        .len = 0,
+    };
 
     esp_err_t ret = httpd_ws_recv_frame(req, &frame, 0);
     if (ret != ESP_OK) {
@@ -112,8 +111,57 @@ void owl_ws_send(const char *message)
     }
 }
 
+static esp_err_t load_index_html()
+{
+    ESP_LOGI(TAG, "Reading index.html");
+    FILE *file = fopen("/spiffs/index.html", "r");
+    if (!file) {
+        ESP_LOGE(TAG, "Failed to open index.html");
+        return ESP_FAIL;
+    }
+
+    fseek(file, 0, SEEK_END);
+    index_html_size = ftell(file);
+    rewind(file);
+
+    index_html = malloc(index_html_size + 1);
+    if (!index_html) {
+        ESP_LOGE(TAG, "Memory allocation for index.html failed");
+        fclose(file);
+        return ESP_ERR_NO_MEM;
+    }
+
+    fread(index_html, 1, index_html_size, file);
+    index_html[index_html_size] = '\0'; // Null-terminate
+    fclose(file);
+
+    return ESP_OK;
+}
+
+static void handle_spiffs()
+{
+    esp_vfs_spiffs_conf_t conf = {
+        .base_path = "/spiffs",
+        .partition_label = NULL,
+        .max_files = 1,
+        .format_if_mount_failed = false,
+    };
+
+    ESP_ERROR_CHECK(esp_vfs_spiffs_register(&conf));
+
+    size_t total = 0, used = 0;
+    ESP_ERROR_CHECK(esp_spiffs_info(NULL, &total, &used));
+    ESP_LOGI(TAG, "Partition size: total: %zu, used: %zu", total, used);
+
+    ESP_ERROR_CHECK(load_index_html());
+
+    ESP_ERROR_CHECK(esp_vfs_spiffs_unregister(NULL));
+    ESP_LOGI(TAG, "SPIFFS unmounted");
+}
+
 void owl_init_http_server()
 {
     server_handle = start_webserver();
+    handle_spiffs();
     ESP_LOGI(TAG, "Initialized HTTP server");
 }
